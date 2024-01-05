8ec0fd7e4eab85ac54df26e4594522427808615d
Alish Rawat <140291603+Alish-Rawat@users.noreply.github.com>
2024-01-05 20:45:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const mongoDB = require("./db");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-allow-Origin", "https://foodiee-mern-api.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-Width, Content-Type,Accept"
